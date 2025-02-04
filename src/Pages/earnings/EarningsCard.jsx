22cadede272a6eaf81fed1b642f5e6c019f28679
ahsan-mahfuz <ahsanmahfuz0@gmail.com>
2025-02-04 17:57:41 +0600

    done all

--- src/Pages/earnings/EarningsCard.jsx
+++ src/Pages/earnings/EarningsCard.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import { CiDollar } from 'react-icons/ci'
+import earningLogo from '../../assets/earnings.svg'
+
 const EarningsCard = () => {
   return (
-    <div className="flex items-center justify-between bg-gradient-to-r h-[300px] w-[500px] from-blue-300 to-green-200 p-6 rounded-lg shadow-lg ">
+    <div className="relative bg-gradient-to-l to-green-[rgba(233,255,233,1)] from-green-100 flex items-center justify-between h-[300px] w-[500px]  p-6 rounded-lg shadow-lg ">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${earningLogo})`,
+          backgroundRepeat: 'no-repeat',
+          backdropFilter: 'blur(20px)',
+          backgroundSize: 'cover',
+          opacity: '1',
+          zIndex: -1,
+          marginTop: '100px',
+          marginLeft: '130px',
+        }}
+      ></div>
+
       <div className="flex items-center space-x-4">
         <div className="bg-white p-3 rounded-full">
           <div className="w-6 h-6 text-blue-500 flex items-center justify-center">
@@ -10,8 +26,8 @@ const EarningsCard = () => {
           </div>
         </div>
         <div>
-          <h3 className="text-black font-semibold text-4xl">Total Earnings</h3>
-          <p className="text-3xl text-blue-800 font-bold ">$23,090</p>
+          <h3 className="text-blue-800 font-bold text-4xl">Total Earnings</h3>
+          <p className="text-4xl  font-bold  ">$23,090</p>
         </div>
       </div>
     </div>
